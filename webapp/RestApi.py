25a09b0365cc38ffe214c24a9cf3c664eb570294
Eran Ohayon <erano067@gmail.com>
2019-05-02 23:14:12 +0300

    server

--- webapp/RestApi.py
+++ webapp/RestApi.py
@@ -59,4 +59,4 @@ def post_synagogue_search():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False, threaded=True)
+    app.run(host='0.0.0.0', port=80)
